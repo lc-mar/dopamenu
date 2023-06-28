cb356d3355cd82a3d4ca2b1993c0b8ac4d1e1ef7
Lauren <78833615+lc-mar@users.noreply.github.com>
2023-06-28 16:55:50 -0400

    building out js

--- js/index.js
+++ js/index.js
@@ -3,3 +3,143 @@ const optionButtonsElement = document.getElementById('option-buttons')
 
 let state = {}
 
+function startGame() {
+    state = {}
+    showTextNode(1)
+  }
+
+  function showTextNode(textNodeIndex) {
+    const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
+    textElement.innerText = textNode.text
+    while (optionButtonsElement.firstChild) {
+      optionButtonsElement.removeChild(optionButtonsElement.firstChild)
+    }
+
+    textNode.options.forEach(option => {
+        if (showOption(option)) {
+            const button = document.createElement('button')
+            button.innerText = option.text
+            button.classList.add('btn')
+            button.addEventListener('click',() => selectOption(option))
+            optionButtonsElement.appendChild(button)
+        }
+
+    })
+}
+
+function showOption(option) {
+    return option.requiredState == null || option.requiredState(state)
+}
+
+function selectOption(option) {
+    const nextTextNodeId = option.nextText
+    if (nextTextNodeId <= 0) {
+        return startGame()
+    }
+    state = Object.assign(state, option.setState)
+    showTextNode(nextTextNodeId)
+}
+
+const textNodes = [
+    {
+        id: 1,
+        text: 'what\'s the vibe?',
+        options: [
+            {
+                text: 'itch brain now',
+                nextText: 2
+            },
+            {
+                text: 'boss bitch shit',
+                nextText: 3
+            },
+            {
+                text: 'more stimmy',
+                nextText: 4
+            },
+            {
+                text: 'rainy day',
+                nextText: 5
+            },
+            {
+                text: 'treat yo self',
+                nextText: 6
+            }
+        ]
+    },
+    {
+        id: 2,
+        text: 'itch brain',
+        options: [
+            {
+                text: 'quotes on whatever you need bb',
+                nextText: 7
+            },
+            {
+                text: 'go watch one episode of a show',
+                nextText: 8
+            },
+            {
+                text: '',
+                nextText: 9
+            },
+            {
+                text: 'do a writing prompt',
+                nextText: 10
+            }
+        ]
+
+    },
+    {
+        id: 3,
+        text: 'boss bitch shit',
+        options: [
+        {
+            text: '15 min productive laur',
+            nextText: 11
+        },
+        {
+            text: 'edit these or rewrite them',
+            nextText: 12
+        },
+        {
+            text: '',
+            nextText: 13
+        },
+        {
+            text: '',
+            nextText: 14
+        }
+    ]
+    },
+    {
+        id: 4,
+        text: 'more stimmy',
+        options: [
+        {
+            text: 'rolling stones top 500 albums you have left',
+            nextText: 11
+        },
+        {
+            text: 'playlists that are lauren study approved',
+            nextText: 12
+        },
+        {
+            text: 'podcasts that are lauren study approved',
+            nextText: 13
+        },
+        {
+            text: 'favorite albums that are lauren approved',
+            nextText: 14
+            },
+        
+        {   text: 'lauren playlist by vibe',
+            nextText: 14
+        }
+    ]
+},
+
+    
+]
+
+startGame()
