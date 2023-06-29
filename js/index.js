0883e4e4b35e7f29d05ae3f20fd36d6bde75f15b
Lauren <78833615+lc-mar@users.noreply.github.com>
2023-06-29 11:24:21 -0400

    more paths, still working, fixed some bugs

--- js/index.js
+++ js/index.js
@@ -6,13 +6,13 @@ let state = {}
 function startGame() {
     state = {}
     showTextNode(1)
-  }
+}
 
-  function showTextNode(textNodeIndex) {
+function showTextNode(textNodeIndex) {
     const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
     textElement.innerText = textNode.text
     while (optionButtonsElement.firstChild) {
-      optionButtonsElement.removeChild(optionButtonsElement.firstChild)
+        optionButtonsElement.removeChild(optionButtonsElement.firstChild)
     }
 
     textNode.options.forEach(option => {
@@ -20,7 +20,7 @@ function startGame() {
             const button = document.createElement('button')
             button.innerText = option.text
             button.classList.add('btn')
-            button.addEventListener('click',() => selectOption(option))
+            button.addEventListener('click', () => selectOption(option))
             optionButtonsElement.appendChild(button)
         }
 
@@ -41,7 +41,9 @@ function selectOption(option) {
 }
 
 const textNodes = [
-    {id: 1,
+    // id 1-10 
+    {
+        id: 1,
         text: 'what\'s the vibe?',
         options: [
             {
@@ -66,7 +68,8 @@ const textNodes = [
             }
         ]
     },
-    {id: 2,
+    {
+        id: 2,
         text: 'itch brain',
         options: [
             {
@@ -88,115 +91,364 @@ const textNodes = [
         ]
 
     },
-    {id: 3,
+    {
+        id: 3,
         text: 'boss bitch shit',
         options: [
-        {
-            text: '15 min productive laur',
-            nextText: 11
-        },
-        {
-            text: 'edit these or rewrite them',
-            nextText: 12
-        },
-        {
-            text: 'BIG tasks',
-            nextText: 13
-        },
-        {
-            text: 'fun but also BBS',
-            nextText: 14
-        }
-    ]
+            {
+                text: '15 min productive laur',
+                nextText: 11
+            },
+            {
+                text: 'edit these or rewrite them',
+                nextText: 12
+            },
+            {
+                text: 'BIG tasks',
+                nextText: 13
+            },
+            {
+                text: 'fun but also BBS',
+                nextText: 14
+            }
+        ]
     },
-    {id: 4,
+    {
+        id: 4,
         text: 'more stimmy',
         options: [
-        {
-            text: 'rolling stones top 500 albums you have left',
-            nextText: 15
-        },
-        {
-            text: 'study buddy',
-            nextText: 16
-        },
-        {
-            text: 'cleaning qween',
-            nextText: 17
-        },
-        {
-            text: 'my faves',
-            nextText: 18
-        }
-    ]
-},
+            {
+                text: 'rolling stones top 500 albums you have left',
+                nextText: 15
+            },
+            {
+                text: 'study buddy',
+                nextText: 16
+            },
+            {
+                text: 'cleaning qween',
+                nextText: 17
+            },
+            {
+                text: 'my faves',
+                nextText: 18
+            }
+        ]
+    },
 
-{id: 5,
-    text: 'rainy day',
-    options: [
     {
-        text: '15 min productive laur',
-        nextText: 19
+        id: 5,
+        text: 'rainy day',
+        options: [
+            {
+                text: '15 min productive laur',
+                nextText: 19
+            },
+            {
+                text: 'edit these or rewrite them',
+                nextText: 20
+            },
+            {
+                text: 'BIG tasks',
+                nextText: 21
+            },
+            {
+                text: 'fun but also BBS',
+                nextText: 22
+            }
+        ]
     },
     {
-        text: 'edit these or rewrite them',
-        nextText: 20
-    },
-    {
-        text: 'BIG tasks',
-        nextText: 21
-    },
-    {
-        text: 'fun but also BBS',
-        nextText: 22
-    }
-]
-    },
-    {   id: 6,
+        id: 6,
         text: 'treat yo self',
         options: [
-        {
-            text: '15 min productive laur',
-            nextText: 11
-        },
-        {
-            text: 'edit these or rewrite them',
-            nextText: 12
-        },
-        {
-            text: 'BIG tasks',
-            nextText: 13
-        },
-        {
-            text: 'fun but also BBS',
-            nextText: 14
-        }
-    ]
+            {
+                text: '15 min productive laur',
+                nextText: 23
+            },
+            {
+                text: 'edit these or rewrite them',
+                nextText: 24
+            },
+            {
+                text: 'BIG tasks',
+                nextText: 25
+            },
+            {
+                text: 'fun but also BBS',
+                nextText: 26
+            }
+        ]
     },
 
     {
         id: 7,
         text: 'what kind of quote you looking for?',
         options: [
-        {
-            text: 'i need to stop being a piece of shit literally this instant',
-            nextText: 23
-        },
-        {
-            text: 'move on ya moody bitch',
-            nextText: 24
-        },
-        {
-            text: 'pretty words',
-            nextText: 25
-        },
-        {
-            text: 'one of your faves',
-            nextText: 26
-        }
-    ]
+            {
+                text: 'i need to stop being a piece of shit literally this instant',
+                nextText: 24
+            },
+            {
+                text: 'move on ya moody bitch',
+                nextText: 25
+            },
+            {
+                text: 'pretty words',
+                nextText: 26
+            },
+            {
+                text: 'one of your faves',
+                nextText: 27
+            }
+        ]
     },
+        {
+          id: 8, // Prior id: 2
+          text: 'what vibe', // Blank text
+          options: [
+            {
+              text: 'funny',
+              nextText: 31 // Leads to id 31
+            },
+            {
+              text: 'one of my go to/s',
+              nextText: 32 // Leads to id 32
+            },
+            {
+              text: 'learn SOMETHING',
+              nextText: 33 // Leads to id 33
+            },
+            {
+              text: 'quick spook',
+              nextText: 34 // Leads to id 34
+            }
+          ]
+        },
+        {
+          id: 9, // Prior id: 2
+          text: '5 min writing prompt', // Blank text
+          options: [
+            {
+              text: 'write a paragraph based on this sentence',
+              nextText: 35 // Leads to id 35
+            },
+            {
+              text: 'you wrote this now expand on it',
+              nextText: 36 // Leads to id 36
+            },
+            {
+              text: 'write a paragraph bio about this character',
+              nextText: 37 // Leads to id 37
+            },
+            {
+              text: 'describe this',
+              nextText: 38 // Leads to id 38
+            }
+          ]
+        },
+        {
+          id: 10, // Prior id: 2
+          text: '', // Blank text
+          options: [
+            {
+              text: '',
+              nextText: 39 // Leads to id 39
+            },
+            {
+              text: '',
+              nextText: 40 // Leads to id 40
+            },
+            {
+              text: '',
+              nextText: 41 // Leads to id 41
+            },
+            {
+              text: '',
+              nextText: 42 // Leads to id 42
+            }
+          ]
+    },
+        // id 11-20
+        {
+          id: 11, // Prior id: 3
+          text: '', // Blank text
+          options: [
+            {
+              text: '',
+              nextText: 43 // Leads to id 43
+            },
+            {
+              text: '',
+              nextText: 44 // Leads to id 44
+            },
+            {
+              text: '',
+              nextText: 45 // Leads to id 45
+            },
+            {
+              text: '',
+              nextText: 46 // Leads to id 46
+            }
+          ]
+        },
+        {
+          id: 12, // Prior id: 3
+          text: '', // Blank text
+          options: [
+            {
+              text: '',
+              nextText: 47 // Leads to id 47
+            },
+            {
+              text: '',
+              nextText: 48 // Leads to id 48
+            },
+            {
+              text: '',
+              nextText: 49 // Leads to id 49
+            },
+            {
+              text: '',
+              nextText: 50 // Leads to id 50
+            }
+          ]
+        },
+        {
+          id: 13, // Prior id: 3
+          text: '', // Blank text
+          options: [
+            {
+              text: '',
+              nextText: 51 // Leads to id 51
+            },
+            {
+              text: '',
+              nextText: 52 // Leads to id 52
+            },
+            {
+              text: '',
+              nextText: 53 // Leads to id 53
+            },
+            {
+              text: '',
+              nextText: 54 // Leads to id 54
+            }
+          ]
+        },
+        {
+          id: 14, // Prior id: 3
+          text: '', // Blank text
+          options: [
+            {
+              text: '',
+              nextText: 55 // Leads to id 55
+            },
+            {
+              text: '',
+              nextText: 56 // Leads to id 56
+            },
+            {
+              text: '',
+              nextText: 57 // Leads to id 57
+            },
+            {
+              text: '',
+              nextText: 58 // Leads to id 58
+            }
+          ]
+    },
+        {
+          id: 15, // Prior id: 4
+          text: '', // Blank text
+          options: [
+            {
+              text: '',
+              nextText: 59 // Leads to id 59
+            },
+            {
+              text: '',
+              nextText: 60 // Leads to id 60
+            },
+            {
+              text: '',
+              nextText: 61 // Leads to id 61
+            },
+            {
+              text: '',
+              nextText: 62 // Leads to id 62
+            }
+          ]
+        },
+        {
+          id: 16, // Prior id: 4
+          text: '', // Blank text
+          options: [
+            {
+              text: '',
+              nextText: 63 // Leads to id 63
+            },
+            {
+              text: '',
+              nextText: 64 // Leads to id 64
+            },
+            {
+              text: '',
+              nextText: 65 // Leads to id 65
+            },
+            {
+              text: '',
+              nextText: 66 // Leads to id 66
+            }
+          ]
+        },
+        {
+          id: 17, // Prior id: 4
+          text: '', // Blank text
+          options: [
+            {
+              text: '',
+              nextText: 67 // Leads to id 67
+            },
+            {
+              text: '',
+              nextText: 68 // Leads to id 68
+            },
+            {
+              text: '',
+              nextText: 69 // Leads to id 69
+            },
+            {
+              text: '',
+              nextText: 70 // Leads to id 70
+            }
+          ]
+        },
+        {
+          id: 18, // Prior id: 4
+          text: '', // Blank text
+          options: [
+            {
+              text: '',
+              nextText: 71 // Leads to id 71
+            },
+            {
+              text: '',
+              nextText: 72 // Leads to id 72
+            },
+            {
+              text: '',
+              nextText: 73 // Leads to id 73
+            },
+            {
+              text: '',
+              nextText: 74 // Leads to id 74
+            }
+          ]
+        },
+      
+     
+      ];
     
-]
 
 startGame()
